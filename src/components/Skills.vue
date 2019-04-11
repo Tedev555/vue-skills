002b9339444309d4be299a5b8e01172efc3c92f8
ted555 <thanong984@gmail.com>
2019-04-11 10:00:26 +0700

    Vue Animation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,94 +1,120 @@
 <template>
   <div class="hello">
     <div class="holder">
-
       <form @submit.prevent="addSkill">
-        <input type="text" placeholder="Enter a skill you have... " v-model="skill" v-validate="'min:5'" name="skill">
-        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
-        {{ skill }}
+        <input
+          type="text"
+          placeholder="Enter a skill you have... "
+          v-model="skill"
+          v-validate="'min:5'"
+          name="skill"
+        >
+
+        <transition name="alert-in">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+        </transition>
       </form>
 
       <ul>
-        <li v-for="(data, index) in skills" :key = 'index'> {{ index }}. {{ data.skill}} </li>
+        <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+          <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill}}</li>
+        </transition-group>
       </ul>
 
-      <p v-if="skills.length >= 1"> You have more than 1 skills</p>
-      <p v-else> You have less than or equal 1 skill</p>
+      <p v-if="skills.length >= 1">You have more than 1 skills</p>
+      <p v-else>You have less than or equal 1 skill</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Skills',
+  name: "Skills",
   data() {
     return {
-      skill: '',
-      skills: [
-        { skill: 'Vue.js'},
-        { skill: 'Frontend Developer'}
-      ],
-    }
+      skill: "",
+      skills: [{ skill: "Vue.js" }, { skill: "Frontend Developer" }]
+    };
   },
   methods: {
     addSkill() {
-      this.$validator.validateAll().then( (result) => {
+      this.$validator.validateAll().then(result => {
         if (result) {
-          this.skills.push({skill: this.skill});
-          this.skill = '';
+          this.skills.push({ skill: this.skill });
+          this.skill = "";
         } else {
-          console.log('Not valid');
+          console.log("Not valid");
         }
       });
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .holder {
-      background: #fff;
-    }
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  }
-  
-  ul li {
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #E0EDF4;
-    border-left: 5px solid #3EB3F6;
-    margin-bottom: 2px;
-    color: #3E5252;
-  }
+.holder {
+  background: #fff;
+}
 
-  p {
-    text-align:center;
-    padding: 30px 0;
-    color: gray;
-  }
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
 
-  .container {
-    box-shadow: 0px 0px 40px lightgray;
-  }
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
 
-  input {
-    width: calc(100% - 40px);
-    border: 0;
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #323333;
-    color: #687F7F;
-  }
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
 
-  .alert {
-    background: #fdf2ce;
-    font-weight: bold;
-    display: inline-block;
-    padding: 5px;
-    margin-top: -20px;
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
+
+.alert-in-enter-active {
+  animation: bounce-in 0.5s;
+}
+.alert-in-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
   }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 </style>
